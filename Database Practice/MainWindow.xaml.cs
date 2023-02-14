a98db37104249239e09c952c01cbb599ce1ed87a
Tiffany Decker <tifjdeck@ut.utm.edu>
2023-02-14 13:59:48 -0600

    connected database

--- Database Practice/MainWindow.xaml.cs
+++ Database Practice/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -23,6 +25,23 @@ namespace Database_Practice
         public MainWindow()
         {
             InitializeComponent();
+            GCNotificationStatus = new OleDbConnection(Provider = Microsoft.ACE.OLEDB.12.0; Data Source =| DataDirectory |\Database_TD.accdb);
+        }
+
+        private void Assetbutton_Click(object sender, RoutedEventArgs e)
+        {
+            OleDbConnection cn;
+            string query = "select * from Assets";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            
+            string data = "";
+
+            while (read.Read())
+            {
+                data += read[0].ToString() + "\n";
+            }
         }
     }
 }
